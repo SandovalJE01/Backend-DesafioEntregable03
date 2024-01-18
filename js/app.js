5be62283b7501aa2e53a8c376e2b00275111762b
Jonathan Ezequiel Sandoval <sandoval.jezequiel01@gmail.com>
2024-01-17 21:44:08 -0300

    testing entregable

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+const express  = require('express'); 
+const productManager = require("./productManager")
+
+const app = express()
+const port = 8000;
+
+app.get('/ping',(req,res) =>{
+    res.send('pong')
+})
+
+app.get('/product',async (req,res) =>{
+    const limit = parseInt(req.query.limit) || null;
+    if(limit){
+        res.send( await productManager.getProductByLimit(limit))
+    }else{
+        res.send( await productManager.getProducts())
+    }
+})
+
+app.get('/product/:id', async (req,res) =>{
+    let id = parseInt(req.params.id);
+    res.send(await productManager.getProductById(id))
+})
+
+app.listen(port,() => console.log('servidor arriba' + Date()))
+
+
+// ? Test
+// http://localhost:8000/product
+// http://localhost:8000/product/1
+// http://localhost:8000/product/?limit=2
